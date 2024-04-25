5ae384cc776a9b8474f5759011bb672c63dd4d29
Neeraj <neerajkumar03788@gmail.com>
2024-04-25 07:08:54 +0530

    setup configured

--- src/app.js
+++ src/app.js
@@ -13,9 +13,31 @@ app.use(cors({
 app.use(express.json({limit:"64kb"})) //json limit
 app.use(express.urlencoded({extended:true, limit:"64kb"})) //urlencode like %20 +
 app.use(express.static("public"))  //pdf images on own server and there is a public folder in our folder whose reference is here
-
-
 app.use(cookieParser())
+
+
+//routes import
+
+import UserRouter from './routes/user.routes.js'
+
+//routes declaration
+/* first we write app.get() but now we have to write app.use() because we seperated 
+router so to bring router we have use middleware app.use() */
+
+
+app.use('/api/v1/users',UserRouter)
+
+//
+
+
+
+
+
+
+
+
+
+
 
 
 export { app }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 // require('dotenv').config({path:'./env'})
-import mongoose from 'mongoose'
+// import mongoose from 'mongoose'
 // import { DB_Name } from './constants';
-import express from 'express'
-import { error } from 'console';
+// import express from 'express'
+
+// import { error } from 'console';
 import connectDB from './db/index.js';
 import dotenv from 'dotenv'
+import { app } from './app.js'
 
-const app=express();
+// const app=express();
 dotenv.config({
     path:'./env'
 })
