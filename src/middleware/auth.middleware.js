700000375758a281b4ff53c38695f12dbd985c02
Neeraj <neerajkumar03788@gmail.com>
2024-04-26 08:46:36 +0530

    login logout created

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
+
+
+
+export const verifyJWT=asyncHandler(async(req,_,next)=>{ 
+    // res have no use so defined with _
+    try {
+        const token = await req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+        
+        if(!token){
+            throw new ApiError(401,"unauthorized request")
+        }
+     
+        const decodedToken=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET);
+        const user = await  User.findById(decodedToken?._id).select("-password -refreshToken")
+    
+       if(!user){
+        //TODO: frontend use
+        throw new ApiError(401,"Invalid Access Token")
+       }
+       req.user = user;
+       next();
+    } catch (error) {
+        throw new ApiError(401,error?.message || "Invalid access Token" )
+    }
+})
